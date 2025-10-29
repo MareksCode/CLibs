92552a840dee41b489a975854977a522484f26c8
MareksCode <marekletsplay@yahoo.com>
2025-10-29 10:01:22 +0100

    udpdate .h

--- library.h
+++ library.h
@@ -2,7 +2,7 @@
 #define MMS_LIB
 
 double lerp(double x1, double x2, double alpha);
-double *scaled(double minimum, double alpha, double *numbers, int numberArrayLength);
+double *scaled(double *numbers, int numberArrayLength, double minimum, double alpha);
 double *createSineArray(double samplingRate, double amp);
 int createArrayFile(double *array, int arrayLength);
 double* readSavedArrayFile();
