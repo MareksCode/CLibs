370a9a3c5f2aa3f240c12beca38aa040d1e4aa73
Marek <119450312+MareksCode@users.noreply.github.com>
2025-11-26 10:07:22 +0100

    stuff

--- MMS25_26.h
+++ MMS25_26.h
@@ -1,13 +1,52 @@
 #ifndef MMS25_26
 #define MMS25_26
+#define BLOCK_SIZE 500
 
-double interpolateDigitsByAlpha(double x1, double x2, double alpha);
-double *interpolate2DPointsByAlpha(double *p1, double *p2, double alpha);
-double *interpolate2DPointsWithX(double *p1, double *p2, double x);
-double getYForXInterpolatedBetween2Points(double x1, double y1, double x2, double y2, double x);
-double *scaleNumbersInArray(double *numbers, int numberArrayLength, double minimum, double alpha);
-double *createSineArray(double samplingRate, double amp);
-int createArrayFile(double *array, int arrayLength);
-double* readSavedArrayFile();
+typedef struct {
+    int binCount;//-1
+    int *bins;//0
+    double minimum;//0
+    double maximum;//0
+    double binWidth;//0
+} Histogram;
 
+typedef struct {
+    int minimumPositionArrayLength;
+    int *minimumPositionArray;
+    int maximumPositionArrayLength;
+    int *maximumPositionArray;
+} LocalExtrema;
+
+typedef struct {
+    int sampleCount;
+    double *samples;
+    double area;
+    double mean;
+    LocalExtrema *localExtrema;
+} MMSignal;
+
+//AUFGABE 1
+double interpolateLine(double x1, double y1, double x2, double y2, double xb);
+double *scaleValuesInArray(int valueArrayLength, double *values, double min, double scalingFactor);
+double *createSineArray(int totalSamples, int samplesPerPeriod, double amplitude);
+int writeArrayFile(char *filePath, double *array, int arrayLength);
+int readArrayFile(char *fileName, double *value);
+MMSignal *createSignal_array(int valueArrayLength, double *values);
+MMSignal *createSignal_file(char *fileName);
+void deleteMMSignal(MMSignal *In);
+void writeSignal(MMSignal *In, char * fileName);
+MMSignal *createSineSignal(int totalSamples, int samplesPerPeriod, double amp);
+
+//AUFGABE 2
+int *getHistogram(int valueCount, double *values, int numBins);
+Histogram *createHistogram_empty();
+Histogram *createHistogram_bins(int binCount);
+Histogram *createHistogram_array(int numValues, double *values, int numBins);
+void deleteHistogram(Histogram *In);
+double computeArea(MMSignal *In);
+double computeMean(MMSignal *In);
+double computeStandardDeviation(MMSignal *In);
+double computeMedian(MMSignal *In);
+LocalExtrema *computeExtrema(MMSignal *In);
+double computeEntropy(Histogram *histogramIn);
 #endif
